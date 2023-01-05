ed65df78baedb3f09c55eabaff3c7d2700bace98
Tathen935 <115939599+Tathen935@users.noreply.github.com>
2023-01-05 18:40:25 +0100

    Update Author.java

--- Author.java
+++ Author.java
@@ -1,4 +1,8 @@
 public class Author {
     public Author(String lastPart, String lastPart2, String vonPart, String firstPart, String jrPart) {
     }
+    
+    public Author getauthor(){
+        return this;
+    }
 }
